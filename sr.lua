34187d1d750dab9706fe2c18ef9420670f43fc29
davidalovell <davidlovell@live.co.uk>
2021-12-24 09:04:08 +0000

    update

--- sr.lua
+++ sr.lua
@@ -1,18 +1,20 @@
 sr = {0,0,0,0,0,0,0,0}
 
 function next(shift_register, next_val)
+
+  table.insert(shift_register, 1, next_val)
+  table.remove(shift_register, #shift_register)
   
   for i = 1, #shift_register do
     play_note(shift_register[i])
   end
   
-  table.insert(shift_register, next_val)
-  table.remove(shift_register, 1)
 end
 
 function play_note(note)
   print(note)
 end
+
 
 
 -- create a function that adds generated note to a table
