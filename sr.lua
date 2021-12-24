0d810f58ee777b871d3ad8f28525517aec6bf9d6
davidalovell <davidlovell@live.co.uk>
2021-12-24 19:37:52 +0000

    update

--- sr.lua
+++ sr.lua
@@ -1,31 +1,47 @@
-scale = {}
-sr = {0,0,0,0,0,0,0,0}
+--- shift register and scale builder from cv
+
+r = {0,0,0,0}
+s = {}
+
+function round(x)
+  return x % 1 >= 0.5 and math.ceil(x) or math.floor(x)
+end
 
 function play_note(note)
   print(note)
 end
 
+function new_cv()
+  -- random pitch cv generator with instability, for testing purposes
+  local random_note = math.random(-23,23)
+  local error_margin = math.random() * math.random(-1,1) / 10
+  return (random_note + error_margin) / 12
+end
 
-function next(shift_register, val, scale_table)
+
+function next(shift_register, scale, val)
+
+  -- convert voltage to nearest semitone, constrain to one octave (12tet)
+  val = round(val * 12) % 12
 
   -- insert new val at start of shift_register
   -- remove val from end of shift_register
   table.insert(shift_register, 1, val)
   table.remove(shift_register, #shift_register)
 
-  -- compare val with existing values in scale_table
+  -- compare val with existing values in scale
   local val_is_duplicate
-  for k, v in ipairs(scale_table) do
+  for k, v in ipairs(scale) do
     if val == v then val_is_duplicate = true end
   end
   
   -- add val if no duplicate value in scale_table
   if not val_is_duplicate then
-    table.insert(scale_table, val)
+    table.insert(scale, val)
   end
 
   -- sort the table in order to produce a scale
-  table.sort(scale_table)
+  table.sort(scale)
 
   -- print shift_register
   for i = 1, #shift_register do
@@ -34,9 +50,9 @@ function next(shift_register, val, scale_table)
 
   print()
 
-  -- print scale_table
-  for i = 1, #scale_table do
-    print(scale_table[i])
+  -- print scale
+  for i = 1, #scale do
+    print(scale[i])
   end
 
 end
@@ -46,9 +62,10 @@ end
 -- create a function that adds generated note to a table -done
 -- remove duplicates -done
 -- sort in order -done
--- multiply cv value x 12
--- round to cv value integer
--- remove transposed octaves - use %
+-- multiply cv value x 12 -done
+-- round to cv value integer -done
+-- remove transposed octaves - use % -done
+-- 
 -- create a pentatonic scale from the notes
 
 -- other ideas which you may or may not wish to add:
